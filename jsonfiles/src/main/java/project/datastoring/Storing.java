ec8c55927d859e2dbb814ddb0055ba1f53d9e38f
sfatew <130330044+sfatew@users.noreply.github.com>
2024-05-20 20:13:27 +0700

    remove old ver

--- jsonfiles/src/main/java/project/datastoring/Storing.java
+++
@@ -1,9 +0,0 @@
-package project.datastoring;
-
-public class Storing {
-
-        public static void storing() {
-            CreateJSONFile createJSONFile = new CreateJSONFile("C:\\Users\\MY LAPTOP\\Downloads\\JSONFilesStorage\\");
-            createJSONFile.createJSONFiles();
-        }
-}
